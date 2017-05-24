6924f81397c85183ec31bcdd581a94770c357985
koocbor <robcook@gmail.com>
2017-05-24 09:01:23 -0400

    start of artsy api calls

---
+++ artsy.js
@@ -0,0 +1,13 @@
+module.exports = function artsy() {
+
+    var artsyCredentials = {
+        clientId: process.env.ARTSY_CLIENT_ID,
+        clientSecret: process.env.ARTSY_CLIENT_SECRET,
+        token: null
+    }
+
+    this.getToken = function() {
+
+    }
+
+}
